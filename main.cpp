f334a2da9069d0ca04a3c05bbf3bd686c9c3755a
leonardoemanuele <86719063+leonardoemanuele@users.noreply.github.com>
2023-04-13 11:43:34 +0200

    Add submodule Pull Down

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <adder.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
   std::cout << "QUANTO FA 15 + 18!?!" << std::endl;
 
   std::cout << add(15.3f, 18.4f) << std::endl;
+
+  GLFWwindow *window;
+  if (!glfwInit())
+  {
+    fprintf(stderr, "Failed to initialize GLFW\n");
+    exit(EXIT_FAILURE);
+  }
+
+  window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+  if (!window)
+  {
+    fprintf(stderr, "Failed to open GLFW window\n");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  // Main loop
+  while (!glfwWindowShouldClose(window))
+  {
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
+
+  // Exit program
+  exit(EXIT_SUCCESS);
 
   return 0;
 }
